feb2bcc6edff4ce9492eb406cf5e8f06a7c88b26
David Ladowitz <david@ladowitz.com>
2016-09-03 00:40:12 -0700

    updating funnel

--- app/models/funnel.rb
+++ app/models/funnel.rb
@@ -11,7 +11,11 @@ class Funnel
     while week_start < @end_date
       week_end = week_start.end_of_week
       week_end = @end_date if week_end > @end_date
-      funnel["#{week_start.strftime "%Y-%m-%d"}-#{week_end.strftime "%Y-%m-%d"}"] = Applicant.where(:updated_at => week_start..week_end).group(:workflow_state).count
+
+      stats = Applicant.where(:updated_at => week_start..week_end).group(:workflow_state).count
+      if stats.any?
+        funnel["#{week_start.strftime "%Y-%m-%d"}-#{week_end.strftime "%Y-%m-%d"}"] = stats
+      end
 
       week_start = week_end.next_week
     end
